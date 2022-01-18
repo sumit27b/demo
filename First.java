f026efc06515f871300d294a87292d09ae70983b
sumit27b <87543002+sumit27b@users.noreply.github.com>
2022-01-18 14:54:59 +0530

    added First.java file

---
+++ First.java
@@ -0,0 +1,15 @@
+//histogrm
+
+public class First
+{
+public static void main(String args[])
+{
+	int num=6;
+	System.out.print(num +":");
+	for(int i=0;i<num;i++)
+	{
+		System.out.print("*");
+	}
+	System.out.println("");
+}
+}
